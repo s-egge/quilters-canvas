65b2bf24d5f4d1aa1bdaf28a5ce3c3294b23d7c9
s-egge <ssteele836@gmail.com>
2024-09-26 11:01:54 -0700

    Mantine initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import '@mantine/core/styles.css'
+import { MantineProvider, createTheme } from '@mantine/core'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const theme = createTheme({
+  /** Mantine theme overrides go here
+   * https://mantine.dev/theming/mantine-provider/
+   * https://mantine.dev/theming/theme-object/
+   */
+})
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MantineProvider theme={theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </MantineProvider>
   )
 }
 
